56b8ca70e12487d497727984792361f05b847379
radha <radhavkrishna1994@gmail.com>
2021-10-26 17:30:00 +0530

    updated

--- ClassesDemo/src/com/training/clients/MyClient.java
+++ ClassesDemo/src/com/training/clients/MyClient.java
@@ -1,10 +1,23 @@
 package com.training.clients;
 
+import com.training.interfaces.MyInterface;
+
 public class MyClient {
 
 	public static void main(String[] args) {
 	
-
+		MyInterface i1 = new MyInterface()
+				{
+					@Override
+					public void genSeries() {
+						for(int i=2;i<=20;i+=2)
+							System.out.print(i+" ");
+						
+					}
+			
+				};
+		
+			i1.genSeries();
 	}
 
 }
