aeecd6174e779095751cb9bccf68a63ede5bc469
radha <radhavkrishna1994@gmail.com>
2021-11-09 13:09:13 +0530

    Spring MVC Demo updated

--- SpringMvcDemo/src/main/java/com/training/controllers/BookController.java
+++ SpringMvcDemo/src/main/java/com/training/controllers/BookController.java
@@ -1,16 +1,26 @@
 package com.training.controllers;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-@RestController
-@RequestMapping("/store")
+@Controller
 public class BookController {
 
-
-	@RequestMapping("/test")
-	public Book test()
+	
+	@RequestMapping("/hello")  // http://localhost:8081/SpringMvcDemo/hello
+	public String sayHello()
 	{
-		return new Book("789","sam");
+		return "demo.jsp";
+	}
+	
+	
+	@ResponseBody
+	@RequestMapping("/welcome")  // http://localhost:8081/SpringMvcDemo/hello
+	public String welcome(@RequestParam("name") String name)
+	{
+		
+		return "welcome to my page:"+name;
 	}
 }
---
+++ SpringMvcDemo/src/main/webapp/demo.jsp
@@ -0,0 +1,21 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Insert title here</title>
+</head>
+<body>
+<h1> Testing Spring UI </h1>
+
+
+<br>
+<form action="welcome">
+Enter name : <input type="text" name="name"/> <br>
+<input type="submit" value="go"/>
+</form>
+
+
+</body>
+</html>
